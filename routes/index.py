c10f3606a71c356db2dec447620379eefb2029e3
chrell99 <chrell99@gmail.com>
2026-02-02 00:11:19 +0100

    refactor: Refactored some of the structure

--- routes/index.py
+++ routes/index.py
@@ -1,13 +1,6 @@
-import os
 from bottle import template
 
 def setup_index(app):
     @app.route('/')
     def home():
-        return template('''
-            % rebase('base.html', title="Home")
-            <div class="grid">
-                <button onclick="location.href='/mediaControl'">Media Control</button>
-                <button onclick="location.href='/config'">Settings / Config</button>
-            </div>
-        ''')
+        return template('index.html')
