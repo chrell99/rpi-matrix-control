4e83cb1c539d8888ac03a8b121732ac4d27fe13e
chrell99 <chrell99@gmail.com>
2026-02-02 00:04:45 +0100

    feat: added new home page and navbar to the top

--- routes/index.py
+++ routes/index.py
@@ -2,4 +2,12 @@ import os
 from bottle import template
 
 def setup_index(app):
-    return template("Defaultpage")
+    @app.route('/')
+    def home():
+        return template('''
+            % rebase('base.html', title="Home")
+            <div class="grid">
+                <button onclick="location.href='/mediaControl'">Media Control</button>
+                <button onclick="location.href='/config'">Settings / Config</button>
+            </div>
+        ''')
