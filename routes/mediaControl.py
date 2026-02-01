7e5661ad0aca60ddc2f8690b439b39e8456b907b
chrell99 <chrell99@gmail.com>
2026-02-02 00:47:11 +0100

    refactor: Refactored mediacontrol and fixed button layout

--- routes/mediaControl.py
+++ routes/mediaControl.py
@@ -20,27 +20,4 @@ def setup_mediaControl(app, media_folder, thumb_folder):
             else:
                 display_items.append({'type': 'image', 'file': f})
 
-        return template('''
-            <!DOCTYPE html>
-            <html>
-            <head>
-                <title>rpi-matrix-control</title>
-                <link rel="stylesheet" href="/static/css/style.css">
-                <link rel="icon" type="image/x-icon" href="/static/images/hoolacane-logo.ico">
-                <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            </head>
-            <body>
-            <main class="grid">
-            % for item in display_items:
-                <button class="media-item" data-file="{{item['file']}}">
-                    % if item['type'] == 'video':
-                        <img src="/thumbnails/{{item['thumb']}}">
-                    % else:
-                        <img src="/media/{{item['file']}}">
-                    % end
-                </button>
-            % end
-            </main>
-            </body>
-            </html>
-        ''', display_items=display_items)
+        return template('mediaControl.html', display_items=display_items)
